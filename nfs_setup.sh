f1ecd5743e54911e50616ac5017858ea1de216e8
oz123 <oz.tiram@gmail.com>
2019-05-06 05:42:39 +0000

    Adapt to Kubernetes

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -2,11 +2,21 @@
 
 set -e
 
-mounts="${@}"
+# read an exports list from an environment variable
+# the list can be separated with a ';' e.g:
+# /path/to/share;/path/to/share2/;path/to/shareN
+if [ -n ${EXPORTS} ]; then
+   IFS=$';'
+fi
+
+# alternatively, accept a mount list from command line
+# e.g. docker run /path/to/share /path/to/share2 /path/to/shareN
+EXPORTS=${EXPORTS:-$@}
+
 
 echo "#NFS Exports" > /etc/exports
 
-for mnt in "${mounts[@]}"; do
+for mnt in ${EXPORTS}; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   mkdir -p $src
   echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
